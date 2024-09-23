0c82e449f18b2979c7571ac888cfded3e7de11aa
diatsilva007 <diatsilva007@gmail.com>
2024-09-23 16:45:50 -0300

    project updated

--- my_script.js
+++ my_script.js
@@ -1,3 +1,8 @@
-alert('Hello, welcome to the course!!!');
+//alert('Hello, welcome to the course!!!');
 
-document.getElementById('nome').value = 'Oi'
+/*
+ Selecione um elemento no Dom
+ Atualizar o valor desse elemento com uma string
+*/
+
+document.getElementById('nome').value = 'Hi'
