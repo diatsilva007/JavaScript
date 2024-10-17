3540ffa3fcfe44d5643ef1e0346b7bbcf5191034
diatsilva007 <diatsilva007@gmail.com>
2024-10-16 23:50:26 -0300

    feat: project updated

--- javascript/app-jogo-mata-mosquito/game.js
+++ javascript/app-jogo-mata-mosquito/game.js
@@ -24,7 +24,7 @@ function posicaoRandomica() {
      // Criar elemento html
      var mosquito = document.createElement('img')
      mosquito.src = 'images/mosquito.png'
-     mosquito.className = 'mosquito1'
+     mosquito.className = tamanhoAleatorio()
      mosquito.style.left = posicaoX + 'px'
      mosquito.style.top = posicaoY + 'px'
      mosquito.style.position = 'absolute'
@@ -32,3 +32,20 @@ function posicaoRandomica() {
      document.body.appendChild(mosquito)
 
 }
+
+// Função para tamanhos diferentes do mosquito na página html
+function tamanhoAleatorio() {
+     var classe = Math.floor(Math.random() * 3)
+     
+     switch(classe) {
+          case 0:
+               return 'mosquito1'
+               
+          case 1:
+               return 'mosquito2'
+               
+          case 2:
+               return 'mosquito3'
+               
+     }
+}    
